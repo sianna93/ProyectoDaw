6f6954fc44b7445ba1e830e835079f1f34714b5a
kleber <kleberdiazc@gmail.com>
2015-09-16 07:35:57 -0500

    no vale esto

--- web/static/js/menu.js
+++ web/static/js/menu.js
@@ -15,9 +15,6 @@ function initialize() {
   document.getElementById('a_buscar').addEventListener('click',F_buscar, false);//BUSCAR
   document.getElementById('a_iniciar_ruta').addEventListener('click',F_iniciaruta, false);
   document.getElementById('a_misrutas').addEventListener('click',F_misrutas, false);
-
-
-
     $(".img_noti_class").click(function()
     {
       $('.listas_notificaciones').remove();
